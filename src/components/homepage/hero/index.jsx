5f1a0f40cc8c087f4fc98a868660ac5907919791
Kaleem Ullah <kaleemullahlakkimarwat123@gmail.com>
2024-02-10 09:46:52 -0800

    backup

--- src/components/homepage/hero/index.jsx
+++ src/components/homepage/hero/index.jsx
@@ -20,7 +20,7 @@ const facilities = [
             <div className="p-16 w-[100%] h-[470px] bg-[white] flex flex-col gap-6">
                 <h1 className="md:text-[48px] text-[30px] font-bold">Limitless learning at your fingertips</h1>
                 <p className="text-sm opacity-70">Online learning and teaching marketplace with 5K+ courses & 10M students. Taught by experts to help you acquire new skills.</p>
-                <div className="bg-[green] md:flex gap-8">
+                <div className="bg-[white] md:flex gap-8">
                     {
                         facilities.map((facility, index)=>(
                             <div className="flex items-center gap-2 py-1 md:py-0" key={index}>
