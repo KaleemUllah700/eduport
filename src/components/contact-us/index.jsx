807120f8d8bea02487f6234e4cd6887ba02f8c98
Kaleem Ullah <kaleemullahlakkimarwat123@gmail.com>
2024-02-12 05:31:39 -0800

    contact component completed

--- src/components/contact-us/index.jsx
+++ src/components/contact-us/index.jsx
@@ -1,9 +1,14 @@
 import Layout from '../shared/layout'
+import Hero from './hero'
+import LetsTalk from './lets-talk'
+import Map from './map'
 const ContactUs = ()=>{
     return (
         <Layout>
             <div>
-                <h1>i am contact us</h1>
+                <Hero />
+                <LetsTalk />
+                <Map />
             </div>
         </Layout>
     )
