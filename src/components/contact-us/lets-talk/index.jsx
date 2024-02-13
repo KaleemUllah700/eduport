6f11664f04770ee10e2a825487155a57425019e6
Kaleem Ullah <kaleemullahlakkimarwat123@gmail.com>
2024-02-13 00:43:22 -0800

    backup

--- src/components/contact-us/lets-talk/index.jsx
+++ src/components/contact-us/lets-talk/index.jsx
@@ -13,17 +13,17 @@ const LetsTalk = ()=>{
                     <form className="flex flex-col gap-4">
                         <div className="flex flex-col gap-2">
                             <label>Fullname*</label>
-                            <input type="text" name="fullname" className="bg-slate-200 p-4 rounded focus:outline-0" />
+                            <input type="text" name="fullname" className="bg-slate-200 p-4 rounded focus:outline-0" required />
                         </div>
 
                         <div className="flex flex-col gap-2">
                             <label>Email*</label>
-                            <input type="email" name="email" className="bg-slate-200 p-4 rounded focus:outline-0" />
+                            <input type="email" name="email" className="bg-slate-200 p-4 rounded focus:outline-0" required />
                         </div>
 
                         <div className="flex flex-col gap-2">
                             <label>Message*</label>
-                            <textarea name="message" className="bg-slate-200 p-4 rounded focus:outline-0" rows={4} />
+                            <textarea name="message" className="bg-slate-200 p-4 rounded focus:outline-0" rows={4} required />
                         </div>
                         <span className='text-end font-bold'><Button>Sent Message</Button></span>
                     </form>
